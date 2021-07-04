6901a15e7e53e31e637f92f93cfb3f0300c916fd
Jorgevvs <jorgevinicius9@gmail.com>
2021-07-04 08:48:24 -0300

    Background switches with panels

--- P1 - Expanding Cards/js/script.js
+++ P1 - Expanding Cards/js/script.js
@@ -1,9 +1,15 @@
 let img = document.querySelectorAll(".container div");
+let body = document.querySelector("body");
 
 img.forEach((imagem) => {
     imagem.addEventListener('click',()=>{
         removerAtivos()
         imagem.classList.add("show")
+        if(imagem.classList.contains('1')) body.style.backgroundColor = "rgb(55, 137, 193)"
+        if(imagem.classList.contains('2')) body.style.backgroundColor = "#1C96A0"
+        if(imagem.classList.contains('3')) body.style.backgroundColor = "#803F2D"
+        if(imagem.classList.contains('4')) body.style.backgroundColor = "#D9C5A6"
+        if(imagem.classList.contains('5')) body.style.backgroundColor = "#0A3679"
     })
 })
 function removerAtivos(){
@@ -11,3 +17,5 @@ function removerAtivos(){
         imagem.classList.remove("show")
     })
 }
+
+
